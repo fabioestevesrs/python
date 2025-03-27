0ff4794768d737be948cdb77ae83f62674f2631f
Fábio Esteves <27526389+fabioestevesrs@users.noreply.github.com>
2025-03-27 14:01:52 -0300

    Adicionando regra de permissão de acesso para react.

--- curso/projeto_fast_api/farm1/api.py
+++ curso/projeto_fast_api/farm1/api.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI
 from routes.jogador import jogador_router
+from fastapi.middleware.cors import CORSMiddleware
+
+client_app = [
+    "http://localhost:3000"
+]
 
 app = FastAPI()
 app.include_router(jogador_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=client_app,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
